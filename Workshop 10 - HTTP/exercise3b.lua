ab5935ad0829ab59d9db05df2cf53a98b297b4c5
Declan Byrd <declan.byrd@gmail.com>
2020-06-02 17:55:00 +0100

    feat: Improved solutions Workshop 10

--- Workshop 10 - HTTP/exercise3b.lua
+++ Workshop 10 - HTTP/exercise3b.lua
@@ -10,8 +10,6 @@ wifi.sta.config(station_cfg)
 
 function crawl()
   url = "http://httpbin.org/ip"
-  --url = "http://www.amazon.co.uk"
-  --url = "http://wttr.in/"
   print(url)
   headers = {
     ["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
@@ -21,6 +19,29 @@ function crawl()
     "GET",
     headers,
     "",
+    function(code, data)
+      if (code < 0) then
+        print("HTTP request failed")
+        print(code)
+      else
+        print(code, data)
+        fileObjWrite = file.open("httpresponse.txt", "w")
+        fileObjWrite:write(data)
+        fileObjWrite:close()
+        post()
+      end
+    end
+  )
+end
+
+function post()
+  fileObjRead = file.open("httpresponse.txt", "r")
+  data = fileObjRead.read()
+  fileObjRead:close()
+  http.post(
+    "http://httpbin.org/post",
+    "Content-Type: text/plain\r\n",
+    data,
     function(code, data)
       if (code < 0) then
         print("HTTP request failed")
