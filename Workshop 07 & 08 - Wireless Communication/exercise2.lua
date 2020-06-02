6e5c281a0c2e98999e76c408c9420a5ce3695f00
Declan Byrd <declan.byrd@gmail.com>
2020-06-02 12:26:37 +0100

    fix(Workshop 7 & 8): sntp sync before cron jobs

--- Workshop 07 & 08 - Wireless Communication/exercise2.lua
+++ Workshop 07 & 08 - Wireless Communication/exercise2.lua
@@ -1,20 +1,55 @@
-cron.schedule(
-    "* * * * *",
-    function(e)
-        print("This function will be executed once every minute")
+local env = require "env"
+
+wifi.sta.autoconnect(1)
+wifi.setmode(wifi.STATION)
+station_cfg = {}
+station_cfg.ssid = env.networkName
+station_cfg.pwd = env.networkPassword
+station_cfg.save = true
+wifi.sta.config(station_cfg)
+
+mytimer = tmr.create()
+
+mytimer:register(
+    3000,
+    1,
+    function()
+        if wifi.sta.getip() == nil then
+            print("Connecting to network...")
+        else
+            sntp.sync(
+                tmsrv,
+                function()
+                    print("Sync success")
+                    mytimer:stop()
+                    cron.schedule(
+                        "* * * * *",
+                        function(e)
+                            print("This function will be executed once every minute")
+                        end
+                    )
+
+                    cron.schedule(
+                        "*/5 * * * *",
+                        function(e)
+                            print("This function will execute once every 5 minutes")
+                        end
+                    )
+
+                    cron.schedule(
+                        "0 22 * * *",
+                        function(e)
+                            print("\n ALARM CLOCK \n IT IS 10PM \n")
+                        end
+                    )
+                end,
+                function()
+                    print("Sync fail")
+                end,
+                1
+            )
+        end
     end
 )
 
-cron.schedule(
-    "*/5 * * * *",
-    function(e)
-        print("This function will execute once every 5 minutes")
-    end
-)
-
-cron.schedule(
-    "0 22 * * *",
-    function(e)
-        print("\n ALARM CLOCK \n IT IS 10PM \n")
-    end
-)
+mytimer:start()
